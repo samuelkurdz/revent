6a4c221b80dff86a055e7234533040f189757519
samuel <kurdzsamuel@gmail.com>
2024-04-08 16:21:29 +0100

    deploying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {DatePipe, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'logo-task';
+  offer2: string[] = ["Money Lending", "Micro-Finance", "Advisory"];
+  currentDate = new Date();
+
+  news = [
+    {
+      title: "A digital prescription for the pharma industry",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo sodales elit......",
+    },
+    {
+      title: "A digital prescription for the pharma industry",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo sodales elit......",
+    },
+    {
+      title: "A digital prescription for the pharma industry",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo sodales elit......",
+    },
+  ];
+
+  bentoContents1=  [
+    {
+      title: "Quick & Instant",
+      description: "We provide you with a faster approach to applying for a loan. As mentioned earlier, you will get your loan within 24 hours.",
+    },
+    {
+      title: "Technology driven",
+      description: "We have enhanced the loan application process by utilizing technology to bring you convenience and speed ",
+    },
+  ]
+  bentoContents2=  [
+    {
+      title: "Competitive interest rates",
+      description: "Our interest rates are hard to beat. With us you get more for less.",
+    },
+    {
+      title: "Convenient & flexible",
+      description: "With us you get access to the funds you need through our hassle-free and efficient processes",
+    },
+  ]
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+          primary: "hsl(var(--primary))",
+          // warning: "hsl(var(--warning))",
+          // "warning-foreground": "hsl(var(--warning-foreground))",
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
+      gridTemplateColumns: {
+        'stats': 'repeat(auto-fill, minmax(min(160px, 100%), 1fr))',
+        // 'stats': 'repeat(auto-fill, minmax(min(160px, 100%), 1fr))',
+      }
+    },
+  },
+  plugins: [],
+}
+
